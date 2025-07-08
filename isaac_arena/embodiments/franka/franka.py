301a637fde9915a8c1695136e6d486ae18fdb053
vramasamy <vramasamy@nvidia.com>
2025-07-08 13:32:25 +0200

    Add dummy code for pick place

---
+++ isaac_arena/embodiments/franka/franka.py
@@ -0,0 +1,49 @@
+from dataclasses import MISSING
+
+from isaaclab.managers import (
+    ObservationGroupCfg as ObsGroup,
+    ObservationTermCfg as ObsTerm,
+)
+from isaaclab.assets.articulation import ArticulationCfg
+from isaaclab.utils import configclass
+from isaaclab_assets.robots.franka import FRANKA_PANDA_HIGH_PD_CFG 
+
+from isaac_arena.embodiments.embodiment import EmbodimentBase
+from . import mdp
+
+
+class Franka(EmbodimentBase):
+    def __init__(self):
+        self.robot = FRANKA_PANDA_HIGH_PD_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
+    
+    @configclass
+    class ActionsCfg:
+        """Action specifications for the MDP."""
+
+        # will be set by agent env cfg
+        arm_action: mdp.JointPositionActionCfg = MISSING
+        gripper_action: mdp.BinaryJointPositionActionCfg = MISSING
+    
+    @configclass
+    class PolicyCfg(ObsGroup):
+        """Observations for policy group with state values."""
+
+        actions = ObsTerm(func=mdp.last_action)
+        joint_pos = ObsTerm(func=mdp.joint_pos_rel)
+        joint_vel = ObsTerm(func=mdp.joint_vel_rel)
+        eef_pos = ObsTerm(func=mdp.ee_frame_pos)
+        eef_quat = ObsTerm(func=mdp.ee_frame_quat)
+        gripper_pos = ObsTerm(func=mdp.gripper_pos)
+
+        def __post_init__(self):
+            self.enable_corruption = False
+            self.concatenate_terms = False
+    
+    def get_robot_cfg(self) -> ArticulationCfg:
+        return self.robot
+
+    def get_action_cfg(self) -> ActionsCfg:
+        return ActionsCfg()
+
+    def get_observation_cfg(self) -> PolicyCfg:
+        return PolicyCfg()
---
+++ isaac_arena/scene/instances/kitchen_mug_pick_and_place_scene.py
@@ -0,0 +1,73 @@
+from typing import Any, Dict
+
+from isaaclab.assets import AssetBaseCfg
+
+from isaac_arena.core.object import Object
+from isaac_arena.scene.pick_and_place_scene import PickAndPlaceSceneBase
+
+
+class KitchenPickAndPlaceScene(PickAndPlaceSceneBase):
+    background_scene = AssetBaseCfg(
+        prim_path="{ENV_REGEX_NS}/Kitchen",
+        # These positions are hardcoded for the kitchen scene. Its important to keep them.
+        init_state=AssetBaseCfg.InitialStateCfg(
+            pos=[0.772, 3.39, -0.895], rot=[0.70711, 0, 0, -0.70711]
+        ),
+        spawn=UsdFileCfg(
+            usd_path=f"omniverse://isaac-dev.ov.nvidia.com/Projects/nvblox/Collected_kitchen_scene/kitchen_scene_teleop_v3.usd"
+        ),
+    )
+    
+    pick_up_object = RigidObjectCfg(
+        prim_path="{ENV_REGEX_NS}/target_mug",
+        init_state=RigidObjectCfg.InitialStateCfg(
+            pos=[0.35, 0.0, 0.094], rot=[0.0, 0.0, 0.0, 1.0]
+        ),
+        spawn=UsdFileCfg(
+            usd_path=f"omniverse://isaac-dev.ov.nvidia.com/Projects/nvblox/Collected_kitchen_scene/mug_physics.usd",
+            scale=(0.0125, 0.0125, 0.0125),
+            activate_contact_sensors=True,
+        ),
+    )
+
+    destination_object = RigidObjectCfg(
+        prim_path="{ENV_REGEX_NS}/bottom_of_drawer_with_mugs",
+        spawn=sim_utils.CuboidCfg(
+            size=[0.4, 0.65, 0.01],
+            rigid_props=sim_utils.RigidBodyPropertiesCfg(kinematic_enabled=True),
+            collision_props=sim_utils.CollisionPropertiesCfg(),
+            activate_contact_sensors=True,
+        ),
+    )
+
+    def __init__(self):
+        super().__init__(
+            background_scene=self.background_scene,
+            pick_up_object=self.pick_up_object,
+            destination_object=self.destination_object,
+        )
+
+    def get_scene_cfg(self) -> Dict[str, Any]:
+        class SceneCfg:
+            background_scene = self.background_scene
+            object = self.destination_object
+
+        return SceneCfg()
+
+    def get_events_cfg(self) -> Dict[str, Any]:
+        class EventsCfg:
+            randomize_pick_up_object = RandomizeObjectCfg(
+                object=self.pick_up_object,
+                assets_list=[
+                    "Object/mug.usd",
+                    "Object/cup.usd",
+                    "Object/bowl.usd",
+                ],
+            )
+            randomize_pick_up_object_position = RandomizeObjectPositionCfg(
+                object=self.pick_up_object,
+                position_std=[0.1, 0.1, 0.1],
+                orientation_std=[0.1, 0.1, 0.1],
+            )
+
+        return EventsCfg()
