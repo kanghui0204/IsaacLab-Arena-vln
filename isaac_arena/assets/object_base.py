7361e942f2f048288309ccbfdbc70c79c2cf08a8
alex <amillane@nvidia.com>
2025-09-08 13:50:47 +0200

    Missing file

---
+++ isaac_arena/assets/object_base.py
@@ -0,0 +1,95 @@
+# Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from abc import ABC, abstractmethod
+from enum import Enum
+from typing import Any
+
+from isaaclab.assets import RigidObjectCfg
+from isaaclab.assets.articulation.articulation_cfg import ArticulationCfg
+from isaaclab.sensors.contact_sensor.contact_sensor_cfg import ContactSensorCfg
+
+from isaac_arena.assets.asset import Asset
+from isaac_arena.geometry.pose import Pose
+
+
+class ObjectType(Enum):
+    ARTICULATION = "articulation"
+    RIGID = "rigid"
+
+
+class ObjectBase(Asset, ABC):
+    """Parent class for (spawnable) Object and ObjectReference."""
+
+    # Defined in Asset, restated here for clariry
+    # name: str | None = None
+    # tags: list[str] | None = None
+
+    def __init__(
+        self,
+        name: str,
+        prim_path: str,
+        initial_pose: Pose | None = None,
+        object_type: ObjectType = ObjectType.RIGID,
+        **kwargs,
+    ):
+        super().__init__(name=name, **kwargs)
+        self.prim_path = prim_path
+        self.object_type = object_type
+        self.initial_pose = initial_pose
+
+    def set_prim_path(self, prim_path: str) -> None:
+        self.prim_path = prim_path
+
+    def get_prim_path(self) -> str:
+        return self.prim_path
+
+    def set_initial_pose(self, pose: Pose) -> None:
+        self.initial_pose = pose
+
+    def get_initial_pose(self) -> Pose | None:
+        return self.initial_pose
+
+    def is_initial_pose_set(self) -> bool:
+        return self.initial_pose is not None
+
+    def get_cfgs(self) -> dict[str, Any]:
+        if self.object_type == ObjectType.RIGID:
+            object_cfg = self._generate_rigid_cfg()
+        elif self.object_type == ObjectType.ARTICULATION:
+            object_cfg = self._generate_articulation_cfg()
+        else:
+            raise ValueError(f"Invalid object type: {self.object_type}")
+        return {
+            self.name: object_cfg,
+        }
+
+    def get_contact_sensor_cfg(self, contact_against_prim_paths: list[str] | None = None) -> ContactSensorCfg:
+        assert self.object_type == ObjectType.RIGID, "Contact sensor is only supported for rigid objects"
+        if contact_against_prim_paths is None:
+            contact_against_prim_paths = []
+        return ContactSensorCfg(
+            prim_path=self.prim_path,
+            filter_prim_paths_expr=contact_against_prim_paths,
+        )
+
+    @abstractmethod
+    def _generate_rigid_cfg(self) -> RigidObjectCfg:
+        # Subclasses must implement this method
+        pass
+
+    @abstractmethod
+    def _generate_articulation_cfg(self) -> ArticulationCfg:
+        # Subclasses must implement this method
+        pass
