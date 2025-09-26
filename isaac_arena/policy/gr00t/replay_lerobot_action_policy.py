aacabed681c60e82d76457bd5c90039caeabf08e
Xinjie Yao <xyao@nvidia.com>
2025-09-26 15:14:40 -0700

    wip

---
+++ isaac_arena/policy/gr00t/replay_lerobot_action_policy.py
@@ -0,0 +1,38 @@
+# Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import gymnasium as gym
+import torch
+from abc import ABC, abstractmethod
+from gymnasium.spaces.dict import Dict as GymSpacesDict
+
+
+class ReplayLerobotActionPolicy(ABC):
+    def __init__(self):
+        """
+        Base class for replay action policies from Lerobot dataset.
+        """
+
+    @abstractmethod
+    def get_action(self, env: gym.Env, observation: GymSpacesDict) -> torch.Tensor:
+        """
+        Compute an action given the environment and observation.
+
+        Args:
+            env: The environment instance.
+            observation: Observation dictionary from the environment.
+
+        Returns:
+            torch.Tensor: The action to take.
+        """
+        pass
