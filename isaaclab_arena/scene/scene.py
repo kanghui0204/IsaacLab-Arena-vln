453f1935f7d010d51ce9e683167bf2a8e1d2ab02
alex <amillane@nvidia.com>
2025-11-26 14:12:55 +0100

    First version working.

--- isaaclab_arena/scene/scene.py
+++ isaaclab_arena/scene/scene.py
@@ -3,13 +3,16 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+import pathlib
 from typing import Any, Union
 
 from isaaclab.assets import AssetBaseCfg, RigidObjectCfg
 from isaaclab.assets.articulation.articulation_cfg import ArticulationCfg
 from isaaclab.sensors.contact_sensor.contact_sensor_cfg import ContactSensorCfg
+from pxr import Gf, Usd, UsdGeom
 
 from isaaclab_arena.assets.asset import Asset
+from isaaclab_arena.assets.object import Object
 from isaaclab_arena.environments.isaaclab_arena_manager_based_env import IsaacLabArenaManagerBasedRLEnvCfg
 from isaaclab_arena.utils.configclass import make_configclass
 
@@ -69,3 +72,50 @@ class Scene:
 
     def modify_env_cfg(self, env_cfg: IsaacLabArenaManagerBasedRLEnvCfg) -> IsaacLabArenaManagerBasedRLEnvCfg:
         return env_cfg
+
+    def export_to_usd(self, output_path: pathlib.Path, root_prim_path: str = "/World") -> None:
+        export_scene_to_usd(self, output_path, root_prim_path)
+
+
+def create_prim_from_asset(stage: Usd.Stage, asset: Object) -> None:
+    """Adds a prim to the stage for the given asset."""
+    assert isinstance(asset, Object)
+    # Get the default prim path
+    default_prim_path = stage.GetDefaultPrim().GetPath()
+    asset_path = str(default_prim_path) + "/" + asset.name
+    prim = stage.DefinePrim(asset_path, "Xform")
+    prim.GetReferences().AddReference(asset.usd_path)
+    # Add the transform
+    prim_xform = UsdGeom.Xform(prim)
+    prim_xform.ClearXformOpOrder()
+    if asset.initial_pose is not None:
+        prim_xform.AddTranslateOp().Set(Gf.Vec3f(asset.initial_pose.position_xyz))
+        prim_xform.AddOrientOp().Set(Gf.Quatf(*asset.initial_pose.rotation_wxyz))
+    prim_xform.AddScaleOp().Set(Gf.Vec3f(asset.scale))
+
+
+def export_scene_to_usd(scene: Scene, output_path: pathlib.Path, root_prim_path: str = "/World") -> None:
+    """Exports the scene to a USD file.
+
+    The resulting USD file will contain a root prim at the given root_prim_path,
+    and each asset in the scene will be added as a child of the root prim.
+    The pose of each asset prim will be set to the initial pose of the asset.
+    Note that the resulting USD is flattened, so all asset references are resolved.
+
+    Args:
+        scene: The scene to export.
+        output_path: The path to the USD file to export to.
+        root_prim_path: The path to the root prim in the USD file.
+    """
+    # Create a new stage for composition
+    stage_out = Usd.Stage.CreateInMemory()
+    # Add the root/default prim
+    world = stage_out.DefinePrim(root_prim_path, "Xform")
+    stage_out.SetDefaultPrim(world)
+    # Add each asset to the stage, under the root prim
+    for asset in scene.assets.values():
+        create_prim_from_asset(stage_out, asset)
+    # Flatten
+    flattened_layer = stage_out.Flatten()
+    # Save to a file
+    flattened_layer.Export(output_path.as_posix())
