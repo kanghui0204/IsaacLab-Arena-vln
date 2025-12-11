b00a943344c1e7a3b9af03adcee981ddfe991f16
alex <amillane@nvidia.com>
2025-12-11 21:13:40 +0100

    bump

--- docs/conf.py
+++ docs/conf.py
@@ -125,6 +125,7 @@ html_css_files = ["custom.css"]
 
 # Versioning
 smv_branch_whitelist = r"^(main|release/.*)$"
+
 smv_tag_whitelist = r"^v.*$"
 html_sidebars = {"**": ["versioning.html", "sidebar-nav-bs"]}
 # Todos
